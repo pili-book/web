52ba95071a37f39df0c4c6993f1952188f2d711b
一页书 <96804858+pili-book@users.noreply.github.com>
2024-07-12 00:06:40 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,79 +1,21 @@
-const { LOCALHOST, tmpLedgerDir } = require('@metaplex-foundation/amman');
-const mplTokenMetadata = require('@metaplex-foundation/mpl-token-metadata');
-const mplCandyMachine = require('@metaplex-foundation/mpl-candy-machine');
-const mplAuctionHouse = require('@metaplex-foundation/mpl-auction-house');
-const mplCandyMachineCore = require('@metaplex-foundation/mpl-candy-machine-core');
-const mplCandyGuard = require('@metaplex-foundation/mpl-candy-guard');
-const path = require('path');
-const MOCK_STORAGE_ID = 'js-next-sdk';
+const { chromium } = require('playwright');
+const browser = await chromium.launch()
+const page = await browser.newPage()
+const navigationPromise = page.waitForNavigation()
 
-function localDeployPath(programName) {
-  return path.join(__dirname, 'programs', `${programName}.so`);
-}
+await page.goto('https://accounts.zoho.com/signin?servicename=VirtualOffice&signupurl=https://www.zoho.com/mail/zohomail-pricing.html&serviceurl=https://mail.zoho.com')
 
-const programs = [
-  {
-    label: 'Token Metadata',
-    programId: mplTokenMetadata.PROGRAM_ADDRESS,
-    deployPath: localDeployPath('mpl_token_metadata'),
-  },
-  {
-    label: 'Candy Machine V2',
-    programId: mplCandyMachine.PROGRAM_ADDRESS,
-    deployPath: localDeployPath('mpl_candy_machine'),
-  },
-  {
-    label: 'Auction House',
-    programId: mplAuctionHouse.PROGRAM_ADDRESS,
-    deployPath: localDeployPath('mpl_auction_house'),
-  },
-  {
-    label: 'Candy Machine V3',
-    programId: mplCandyMachineCore.PROGRAM_ADDRESS,
-    deployPath: localDeployPath('mpl_candy_machine_core'),
-  },
-  {
-    label: 'Candy Guard',
-    programId: mplCandyGuard.PROGRAM_ADDRESS,
-    deployPath: localDeployPath('mpl_candy_guard'),
-  },
-  {
-    label: 'Gateway',
-    programId: 'gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs',
-    deployPath: localDeployPath('solana_gateway_program'),
-  },
-  {
-    label: 'Token Auth Rules',
-    programId: 'auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg',
-    deployPath: localDeployPath('mpl_token_auth_rules'),
-  },
-];
+await page.setViewportSize({ width: 1374, height: 880 })
 
-module.exports = {
-  validator: {
-    killRunningValidators: true,
-    programs,
-    jsonRpcUrl: LOCALHOST,
-    websocketUrl: '',
-    commitment: 'confirmed',
-    ledgerDir: tmpLedgerDir(),
-    resetLedger: true,
-    verifyFees: false,
-  },
-  relay: {
-    accountProviders: {
-      ...mplTokenMetadata.accountProviders,
-      ...mplCandyMachine.accountProviders,
-      // ...mplAuctionHouse.accountProviders,
-      ...mplCandyMachineCore.accountProviders,
-      ...mplCandyGuard.accountProviders,
-    },
-  },
-  storage: {
-    storageId: MOCK_STORAGE_ID,
-    clearOnStart: true,
-  },
-  snapshot: {
-    snapshotFolder: path.join(__dirname, 'snapshots'),
-  },
-};
+await page.waitForSelector('#login_id')
+await page.click('#login_id')
+
+await page.waitForSelector('#password')
+await page.click('#password')
+
+await page.waitForSelector('#nextbtn')
+await page.click('#nextbtn')
+
+await navigationPromise
+
+await browser.close()
