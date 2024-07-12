82f37c7c5738fd1077b332524421956c9900f61d
一页书 <96804858+pili-book@users.noreply.github.com>
2024-07-12 09:06:32 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,21 +1,13 @@
-const { chromium } = require('playwright');
-const browser = await chromium.launch()
-const page = await browser.newPage()
-const navigationPromise = page.waitForNavigation()
+const init = async () => {
+  // 启动浏览器
+  const browser = await puppeteer.launch({
+    headless: false, // 是否以无头模式运行，默认为 true，除非选择开启了开发者工具
+    devtools: true, // 是否自动打开开发者工具，默认为 false
+    args: ["--start-maximized"], // 启动时传递给浏览器的额外参数
+  });
+const page = await browser.newPage();
+  // 跳转到相应的网站
+  await page.goto("https://example.com");
 
-await page.goto('https://accounts.zoho.com/signin?servicename=VirtualOffice&signupurl=https://www.zoho.com/mail/zohomail-pricing.html&serviceurl=https://mail.zoho.com')
-
-await page.setViewportSize({ width: 1374, height: 880 })
-
-await page.waitForSelector('#login_id')
-await page.click('#login_id')
-
-await page.waitForSelector('#password')
-await page.click('#password')
-
-await page.waitForSelector('#nextbtn')
-await page.click('#nextbtn')
-
-await navigationPromise
-
-await browser.close()
+  // 关闭浏览器
+  await browser.close();
